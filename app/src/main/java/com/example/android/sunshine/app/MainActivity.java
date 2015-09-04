01a856bba8880b7fe0f84bc28eb8d559b29616fe
Martin Laskowski <martinlaskowski@me.com>
2015-09-04 15:54:45 -0400

    2.12 Move ForecastFragment to new .java file

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -3,26 +3,9 @@ package com.example.android.sunshine.app;
 
 // Import statements make other packages, classes or interfaces available to this piece of code (which in turn becomes a new class or interface)
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends ActionBarActivity { // We add an ActionBar to our activity (from API 7 or higher) by extending the now deprecated ActionBarActivity and setting the activity theme to Theme.AppCompat or similar.
 
@@ -55,117 +38,5 @@ public class MainActivity extends ActionBarActivity { // We add an ActionBar to 
         // the can presumably be other if statements here testing for other menu items we create
 
         return super.onOptionsItemSelected(item); // return the id of the item selected
-    }
-
-
-
-
-    // defines a fragment (class) called ForecastFragment containing a simple view that is instantiated above in the onCreate method
-    public static class ForecastFragment extends Fragment {
-
-        public ForecastFragment() { // nothing defined here
-        }
-
-        // instantiates an ArrayAdapter for strings called ForecastAdapter, which gets called further down below
-        private ArrayAdapter<String> ForecastAdapter;
-
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) { // onCreateView creates and returns the view hierarchy associated with the fragment
-            View rootView = inflater.inflate( // creates a new View object called rootView that is inflated from fragment_main.xml
-                    R.layout.fragment_main, // parser ... XML dom node containing the description of the view hierarchy
-                    container, // root ... Optional view to be the parent of the generated hierarchy (if attachToRoot is true), or else simply an object that provides a set of LayoutParams values for root of the returned hierarchy (if attachToRoot is false.)
-                    false); // attachToRoot	... Whether the inflated hierarchy should be attached to the root parameter? If false, root is only used to create the correct subclass of LayoutParams for the root view in the XML.
-
-            // creates an array of manually entered strings called fakeForecastDataArray
-            String[] fakeForecastDataArray = {
-                    "Today - Sunny - 88 / 63",
-                    "Tomorrow - Foggy - 70 / 46",
-                    "Weds - Cloudy - 72 / 63",
-                    "Thurs - Rainy - 64 / 51",
-                    "Fri - Foggy - 70 / 46",
-                    "Sat - Sunny - 76 - 68"
-            };
-
-            // creates a new List object for storing Strings called weekForecast. This object then is made equal to (populated by) ...
-            List<String> weekForecast = new ArrayList<String>( // ... an ArrayList of Strings that contains ...
-                    Arrays.asList(fakeForecastDataArray) // ... our fakeForecastDataArray as a list
-            );
-
-
-            // (calls) populates that ForecastAdapter instantiated above with that List of Strings called weeForecast that's full of our dummy data right now
-            ForecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast); // (context, ID of layout to populate, ID of TextView to take shape parameters from, the data array)
-
-            // finds (by ID) the ListView we want to link to our new ArrayAdapter created above
-            ListView listView = (ListView) rootView.findViewById(R.id.list_view_forecast);
-
-            // sets our ArrayAdapter onto the listView!!!
-            listView.setAdapter(ForecastAdapter);
-
-
-
-            // THIS SNIPPET is taken from the GitGist page at https://gist.github.com/udacityandroid/d6a7bb21904046a91695 and adds the networking code.
-
-            // These two need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-            // Will contain the raw JSON response as a string.
-            String forecastJsonStr = null;
-
-            try {
-                // Construct the URL for the OpenWeatherMap query
-                // Possible parameters are avaiable at OWM's forecast API page, at
-                // http://openweathermap.org/API#forecast
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
-
-                // Create the request to OpenWeatherMap, and open the connection
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                forecastJsonStr = buffer.toString();
-            } catch (IOException e) {
-                Log.e("ForecastFragment", "Error ", e);
-                // If the code didn't successfully get the weather data, there's no point in attemping
-                // to parse it.
-                return null;
-            } finally{
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e("ForecastFragment", "Error closing stream", e);
-                    }
-                }
-            }
-
-            return rootView;
-        }
     }
 }

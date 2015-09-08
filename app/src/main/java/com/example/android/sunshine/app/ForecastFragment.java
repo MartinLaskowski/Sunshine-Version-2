5d776709270a4f38c2333cc54de8327484321f6f
Martin Laskowski <martinlaskowski@me.com>
2015-09-08 13:40:41 -0400

    3.21 Feat: Add Units setting to SettingsActivity

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -107,14 +107,12 @@ public class ForecastFragment extends Fragment {
         String location = sharedPref.getString(getString(R.string.pref_location_key),
                 getString(R.string.pref_location_default));
         weatherTask.execute(location);
-
     }
 
     @Override
     public void onStart() {
         super.onStart();
         updateWeather();
-
     }
 
     public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {

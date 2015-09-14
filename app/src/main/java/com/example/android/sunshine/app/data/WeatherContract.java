02a780bae5d38a2a06954e034eded04926fb8d2f
Martin Laskowski <martinlaskowski@me.com>
2015-09-13 20:03:14 -0400

    4A.23 Feat: Add columns to Location table in WeatherContract, ready some tests

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
@@ -18,6 +18,14 @@ public class WeatherContract {
     public static final class LocationEntry implements BaseColumns {
         public static final String TABLE_NAME = "location";
 
+        public static final String COLUMN_LOCATION_SETTING = "location_setting"; // zip code setting, sent to OWM (real)
+
+        public static final String COLUMN_CITY_NAME = "city_name"; // city name (text)
+
+        public static final String COLUMN_COORD_LAT = "coord_lat"; // (real)
+
+        public static final String COLUMN_COORD_LONG = "coord_long"; // (real)
+
     }
 
     // Inner class defining the table contents of the Weather table
